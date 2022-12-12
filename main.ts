9307a9a8613f55186d87fcb98d7c08c95c2750e7
Kzvuong <118218278+Kzvuong@users.noreply.github.com>
2022-12-12 13:20:30 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,56 +1,28 @@
 let burger: Sprite = null
 let monkey = sprites.create(img`
-    . . . . f f f f f . . . . . . . 
-    . . . f e e e e e f . . . . . . 
-    . . f d d d d e e e f . . . . . 
-    . c d f d d f d e e f f . . . . 
-    . c d f d d f d e e d d f . . . 
-    c d e e d d d d e e b d c . . . 
-    c d d d d c d d e e b d c . . . 
-    c c c c c d d e e e f c . . . . 
-    . f d d d d e e e f f . . . . . 
-    . . f f f f f e e e e f . . . . 
-    . . . . f f e e e e e e f . f f 
-    . . . f e e f e e f e e f . e f 
-    . . f e e f e e f e e e f . e f 
-    . f b d f d b f b b f e f f e f 
-    . f d d f d d f d d b e f f f f 
-    . . f f f f f f f f f f f f f . 
+    . f f f . 
+    . f f f . 
+    . f f f . 
+    . . f . . 
+    . f f f . 
+    f . f . f 
+    . . f . . 
+    . . f . . 
+    . f . f . 
+    f . . . f 
     `, SpriteKind.Player)
 controller.moveSprite(monkey)
 game.onUpdateInterval(500, function () {
     burger = sprites.create(img`
-        ...........ccccc66666...........
-        ........ccc4444444444666........
-        ......cc444444444bb4444466......
-        .....cb4444bb4444b5b444444b.....
-        ....eb4444b5b44444b44444444b....
-        ...ebb44444b4444444444b444446...
-        ..eb6bb444444444bb444b5b444446..
-        ..e6bb5b44444444b5b444b44bb44e..
-        .e66b4b4444444444b4444444b5b44e.
-        .e6bb444444444444444444444bb44e.
-        eb66b44444bb444444444444444444be
-        eb66bb444b5b44444444bb44444444be
-        fb666b444bb444444444b5b4444444bf
-        fcb666b44444444444444bb444444bcf
-        .fbb6666b44444444444444444444bf.
-        .efbb66666bb4444444444444444bfe.
-        .86fcbb66666bbb44444444444bcc688
-        8772effcbbbbbbbbbbbbbbbbcfc22778
-        87722222cccccccccccccccc22226678
-        f866622222222222222222222276686f
-        fef866677766667777776667777fffef
-        fbff877768f86777777666776fffffbf
-        fbeffeefffeff7766688effeeeefeb6f
-        f6bfffeffeeeeeeeeeeeeefeeeeebb6e
-        f66ddfffffeeeffeffeeeeeffeedb46e
-        .c66ddd4effffffeeeeeffff4ddb46e.
-        .fc6b4dddddddddddddddddddb444ee.
-        ..ff6bb444444444444444444444ee..
-        ....ffbbbb4444444444444444ee....
-        ......ffebbbbbb44444444eee......
-        .........fffffffcccccee.........
-        ................................
+        . 2 2 2 . 
+        . 2 2 2 . 
+        . 2 2 2 . 
+        . . 2 . . 
+        . 2 2 2 . 
+        2 . 2 . 2 
+        . . 2 . . 
+        . . 2 . . 
+        . 2 . 2 . 
+        2 . . . 2 
         `, SpriteKind.Player)
 })
